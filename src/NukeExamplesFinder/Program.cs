00f5baac068b1bde36c3fff8365921633940a6b5
Volkmar Rigo <volkmar@rigo.bz>
2020-04-13 17:18:21 +0200

    Added DI and configuration

--- src/NukeExamplesFinder/Program.cs
+++ src/NukeExamplesFinder/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace NukeExamplesFinder
 {
     class Program
     {
-        static void Main(string[] args)
+        public IConfiguration Configuration { get; }
+
+        public Program(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Run()
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Configuration.GetValue<string>("Credentials:GitHubToken"));
+        }
+
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder()
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddLogging(configure => configure.AddConsole());
+                   services.AddTransient<Program>();
+
+               })
+               .ConfigureAppConfiguration((hostContext, builder) =>
+               {
+                   builder
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile($"appsettings.json", optional: false);
+                   
+                   if (string.Equals(Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT") ?? "development", "development", StringComparison.OrdinalIgnoreCase))
+                       builder.AddUserSecrets<Program>();
+               })
+               .UseConsoleLifetime();
+
+            var host = builder.Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                try
+                {
+                    services.GetRequiredService<Program>().Run();
+                }
+                catch (Exception ex)
+                {
+                    host.Services.GetRequiredService<ILogger<Program>>().LogError(ex, "Error Occured");
+                }
+            }
         }
     }
 }
