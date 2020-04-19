624e8d634207e770e93e9e8b7fbd4046b32c3b13
volkmar <volkmar.rigo@infominds.eu>
2020-04-19 09:19:33 +0200

    Moved HostBuilder Setup to startup class

--- src/NukeExamplesFinder/Program.cs
+++ src/NukeExamplesFinder/Program.cs
@@ -14,56 +14,21 @@ namespace NukeExamplesFinder
     {
         static void Main(string[] args)
         {
-            var builder = new HostBuilder()
-               .ConfigureAppConfiguration((hostContext, builder) =>
-               {
-                   builder
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile($"appsettings.json", optional: false);
+            var host = Startup.CreateHostBuilder().Build();
 
-                   if (string.Equals(Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT") ?? "development", "development", StringComparison.OrdinalIgnoreCase))
-                       builder.AddUserSecrets<Program>();
-               })
-               .ConfigureServices((hostContext, services) =>
-               {
-                   services.AddLogging(configure => configure.AddConsole());
-                   services.AddTransient<ExampleFinderService>();
-                   services.AddTransient<RepositoryListService>();
-                   services.AddTransient<IGitHubGateway, GitHubGateway>();
-                   services.AddTransient<IFileGateway, FileGateway>();
-                   services.AddGitHubClient();
-               })
-               .UseConsoleLifetime();
+            using var serviceScope = host.Services.CreateScope();
+            var services = serviceScope.ServiceProvider;
 
-            var host = builder.Build();
-
-            using (var serviceScope = host.Services.CreateScope())
+            try
             {
-                var services = serviceScope.ServiceProvider;
-
-                try
-                {
-                    services.GetRequiredService<RepositoryListService>().ExecuteAsync().GetAwaiter().GetResult();
-                }
-                catch (Exception ex)
-                {
-                    host.Services.GetRequiredService<ILogger<ExampleFinderService>>().LogError(ex, "Error Occured");
-                }
+                services.GetRequiredService<RepositoryListService>().ExecuteAsync().GetAwaiter().GetResult();
+                services.GetRequiredService<RenderService>().Execute();
+            }
+            catch (Exception ex)
+            {
+                services.GetRequiredService<ILogger<RepositoryListService>>().LogError(ex, "Error Occured");
             }
         }
-    }
-    static class ServiceRegistration
-    {
-        public static IServiceCollection AddGitHubClient(this IServiceCollection collection)
-        {
-            return collection.AddTransient<IGitHubClient>((serviceProvider) =>
-            {
-                var token = serviceProvider.GetRequiredService<IConfiguration>().GetValue<string>("Credentials:GitHubToken");
-                if (string.IsNullOrEmpty(token))
-                    throw new ArgumentException("Credentials:GitHubToken can not be empty");
 
-                return new GitHubClient(new ProductHeaderValue("NukeExampleFinder")) { Credentials = new Credentials(token) };
-            });
-        }
     }
 }
---
+++ src/NukeExamplesFinder/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NukeExamplesFinder.Gateways;
+using NukeExamplesFinder.Services;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NukeExamplesFinder
+{
+    public static class Startup
+    {
+        public static IHostBuilder CreateHostBuilder()
+            => new HostBuilder()
+                .ConfigureAppConfiguration(ConfigureAppConfiguration)
+                .ConfigureServices(ConfigureServices)
+                .UseConsoleLifetime();
+
+        public static void ConfigureAppConfiguration(HostBuilderContext hostContext, IConfigurationBuilder builder)
+        {
+            builder
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile($"appsettings.json", optional: false);
+
+            if (string.Equals(Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT") ?? "development", "development", StringComparison.OrdinalIgnoreCase))
+                builder.AddUserSecrets<Program>();
+        }
+
+        public static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole());
+            services.AddTransient<RenderService>();
+            services.AddTransient<RepositoryListService>();
+            services.AddTransient<IGitHubGateway, GitHubGateway>();
+            services.AddTransient<IFileGateway, FileGateway>();
+
+            services.AddTransient<IGitHubClient>((serviceProvider) =>
+            {
+                var token = serviceProvider.GetRequiredService<IConfiguration>().GetValue<string>("Credentials:GitHubToken");
+                if (string.IsNullOrEmpty(token))
+                    throw new ArgumentException("Credentials:GitHubToken can not be empty");
+
+                return new GitHubClient(new ProductHeaderValue("NukeExampleFinder")) { Credentials = new Credentials(token) };
+            });
+        }
+
+    }
+}
