8c72523b8362b04ce3e63760527aca6a05da3dda
volkmar <volkmar.rigo@infominds.eu>
2020-04-25 16:06:57 +0200

    First version with git action

--- build/Build.cs
+++ build/Build.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using Nuke.Common;
+using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
@@ -17,12 +18,16 @@ using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-[GitHubActions("Run", GitHubActionsImage.WindowsLatest, OnCronSchedule = "* 12 * * *")]
+[GitHubActions(
+    nameof(Run),
+    GitHubActionsImage.WindowsLatest, 
+    On = new GitHubActionsTrigger[] { GitHubActionsTrigger.Push }, 
+    InvokedTargets = new string[] { nameof(Run) })]
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
 class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Run);
+    public static int Main() => Execute<Build>(x => x.Run);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -32,14 +37,14 @@ class Build : NukeBuild
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
-
+    AbsolutePath RunOutputDirectory => RootDirectory / "output";
     Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(ArtifactsDirectory);
-        });
+    .Before(Restore)
+    .Executes(() =>
+    {
+        SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+        EnsureCleanDirectory(ArtifactsDirectory);
+    });
 
     Target Restore => _ => _
         .Executes(() =>
@@ -61,9 +66,10 @@ class Build : NukeBuild
 
     Target Run => _ => _
         .DependsOn(Compile)
+        .Produces(RunOutputDirectory / "Directory.md", RunOutputDirectory / "Repos.json")
         .Executes(() =>
         {
-            var settings = new { Credentials = new { GitHubToken = "" }, DataFiles = new { Path = ArtifactsDirectory / "XXX" } };
+            var settings = new { Credentials = new { GitHubToken = "" }, DataFiles = new { Path = RunOutputDirectory } };
             File.WriteAllText(ArtifactsDirectory / "appsettings.json", JsonConvert.SerializeObject(settings));
 
             Tool tool = ToolResolver.GetLocalTool(ArtifactsDirectory / "NukeExamplesFinder.exe");
